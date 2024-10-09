4d139982afc1064b33a42d244cb9b052f33232b8
double-002 <oletjelta@gmail.com>
2024-10-09 13:04:18 +0200

    importing values

--- plot.py
+++ plot.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from Fil1_Aapning import dato_tid1_dt , temperatur1
+from data_les_MET import Lufttemp_MET
+
 
 temp_1 = []
 temp_2 = []
@@ -12,9 +15,9 @@ def temperature_average(temp, n):
         window_average = sum(window) / n
         temp_avg.append(window_average)
 
-plt.plot(temp_1,datetime, label = "Temperatur 1")
-plt.plot(temp_2,datetime, label = "Temperatur 2")
-plt.plot(temp_avg,datetime, label = "Gjennomsnittstemperatur")
+plt.plot(temp_1,datetime, label = "Temperatur", color = "b")
+plt.plot(temp_2,datetime, label = "Temperatur MET", color = "g")
+plt.plot(temp_avg,datetime, label = "Gjennomsnittstemperatur", color = "o")
 
 plt.ylabel("Temperatur i °C")
 plt.legend()
