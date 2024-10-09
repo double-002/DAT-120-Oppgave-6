e4e73d695930425c4ac3f663690503950debc371
double-002 <oletjelta@gmail.com>
2024-10-09 13:42:15 +0200

    la til fillagring

--- plot.py
+++ plot.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 from Fil1_Aapning import dato_tid1_dt , temperatur1
 from data_les_MET import Lufttemp_MET
 
-#print(dato_tid1_dt[0])
-#print(temperatur1[0])
-#print(Lufttemp_MET[0])
-
+temp_avg = list()
 def temperature_average(temp, n):
     for i in range(len(temp)- n +1):
         window = temp[i:i+n]
@@ -19,7 +17,8 @@ temp_avg = temperature_average(Lufttemp_MET, 30)
 plt.plot(temperatur1,dato_tid1_dt, label = "Temperatur", color = "blue")
 plt.plot(Lufttemp_MET,dato_tid1_dt, label = "Temperatur MET", color = "green")
 plt.plot(temp_avg,dato_tid1_dt, label = "Gjennomsnittstemperatur", color = "orange")
-
+plt.axis(0o6-11, 0o6-14, 8, 24)
 plt.ylabel("Temperatur i °C")
 plt.legend()
-plt.show
+plt.savefig("Temperatur_plot.png")
+plt.show()
