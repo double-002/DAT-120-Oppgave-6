d49836e649992c51f20762ed1eaac527e68dba01
double-002 <oletjelta@gmail.com>
2024-10-09 13:19:05 +0200

    variabler klare

--- plot.py
+++ plot.py
@@ -3,11 +3,9 @@ import numpy as np
 from Fil1_Aapning import dato_tid1_dt , temperatur1
 from data_les_MET import Lufttemp_MET
 
-
-temp_1 = []
-temp_2 = []
-datetime = []
-temp_avg = []
+#print(dato_tid1_dt[0])
+#print(temperatur1[0])
+#print(Lufttemp_MET[0])
 
 def temperature_average(temp, n):
     for i in range(len(temp)- n +1):
@@ -15,9 +13,12 @@ def temperature_average(temp, n):
         window_average = sum(window) / n
         temp_avg.append(window_average)
 
-plt.plot(temp_1,datetime, label = "Temperatur", color = "b")
-plt.plot(temp_2,datetime, label = "Temperatur MET", color = "g")
-plt.plot(temp_avg,datetime, label = "Gjennomsnittstemperatur", color = "o")
+temp_avg = temperature_average(Lufttemp_MET, 30)
+
+
+plt.plot(temperatur1,dato_tid1_dt, label = "Temperatur", color = "blue")
+plt.plot(Lufttemp_MET,dato_tid1_dt, label = "Temperatur MET", color = "green")
+plt.plot(temp_avg,dato_tid1_dt, label = "Gjennomsnittstemperatur", color = "orange")
 
 plt.ylabel("Temperatur i °C")
 plt.legend()
