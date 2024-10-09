03d709cf9e7513cd3da2c877eae8e10d78f49c6a
Joachim <jmonsen03@gmail.com>
2024-10-09 10:36:45 +0200

    Skapt fil leser for trykk_og_temperaturlogg_rune_time

---
+++ Fil1_Aapning.py
@@ -0,0 +1,49 @@
+import datetime as dt
+
+
+#startverdi = 0
+sluttverdi = 45700
+
+
+#startverdi_str = str(startverdi)
+sluttverdi_str = str(sluttverdi+10)
+    
+def fil1data(sluttverdi_str):
+    
+    tid_fra_start1 = list()
+    dato_tid1_str = list()
+    dato_tid1_dt = list()
+    trykk_bar1 = list()
+    abs_trykk1 = list()
+    temperatur1 = list() 
+    
+    fil1 = open('trykk_og_temperaturlogg_rune_time.csv', mode='r')
+    
+    for linjer in fil1:
+        split = linjer.split(';')
+        split[4] = split[4].replace('\n', '')
+        dato_tid1_str.append(split[0])
+        tid_fra_start1.append(split[1])
+        trykk_bar1.append(split[2])
+        abs_trykk1.append(split[3])
+        temperatur1.append(split[4])
+            
+        if split[1] == sluttverdi_str: 
+            tid_fra_start1.pop(0)
+            trykk_bar1.pop(0)
+            abs_trykk1.pop(0)
+            temperatur1.pop(0)
+            dato_tid1_str.pop(0)
+            
+            for x in range(len(dato_tid1_str)):
+                dato_tid1_dt.append(dt.datetime.strptime(dato_tid1_str[x], "%m.%d.%Y %H:%M"))
+                
+            fil1.close()
+            
+            return tid_fra_start1, dato_tid1_str, dato_tid1_dt, trykk_bar1, abs_trykk1, temperatur1
+
+tid_fra_start1, dato_tid1_str, dato_tid1_dt, trykk_bar1, abs_trykk1, temperatur1 = fil1data(sluttverdi_str)
+
+
+
+
